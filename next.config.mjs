554f49ccc670425df485daac5e503861aba6fe86
stanley <stanleyagbanobi@gmail.com>
2024-09-22 19:15:32 +0100

    next.config and getStripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' https://js.stripe.com; frame-src 'self' https://js.stripe.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
