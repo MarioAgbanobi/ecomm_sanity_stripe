d80a815bb7c0ba51270c0c034eb7987b6ae7ef86
stanley <stanleyagbanobi@gmail.com>
2024-09-22 18:17:17 +0100

    succes fixed

--- pages/success.js
+++ pages/success.js
@@ -1,47 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react';
 import Link from 'next/link';
-import { BsBagCheckFill } from 'react-icons/bs'
+import { BsBagCheckFill } from 'react-icons/bs';
 import { useStateContext } from '@/context/StateContext';
 import { runFireworks } from '@/lib/utils';
 
-const success = () => {
+const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
-    useEffect(()=> {
+    useEffect(() => {
         localStorage.clear();
-
         setCartItems([]);
         setTotalPrice(0);
         setTotalQuantities(0);
         runFireworks();
-    }, [])
+    }, []);
 
-  return (
-    <div className='success-wrapper'>
-        <div className='success'>
-
-            <p className='icon'>
-                <BsBagCheckFill />
-            </p>
-            <h2>Thank you for your order!</h2>
-
-            <p className='email-msg'>Check your email inbox for the receipt</p>
-
-            <p className='description'>
-                If you have any questions, please email
-                <a className='email' href='mailto:order@example.com'>
-                    order@example.com
-                </a>
-            </p>
-
-            <Link href='/'>
-                <button type='button' width='300px' className='btn'>
-                    Continue Shopping
-                </button>
-            </Link>
+    return (
+        <div className="success-wrapper">
+            <div className="success">
+                <p className="icon">
+                    <BsBagCheckFill />
+                </p>
+                <h2>Thank you for your order!</h2>
+                <p className="email-msg">Check your email inbox for the receipt</p>
+                <p className="description">
+                    If you have any questions, please email
+                    <a className="email" href="mailto:order@example.com">
+                        order@example.com
+                    </a>
+                </p>
+                <Link href="/">
+                    <button type="button" width="300px" className="btn">
+                        Continue Shopping
+                    </button>
+                </Link>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default success
+export default Success;
