c9e5f2a9770efbfe27a2c26d662919188ab3a8aa
stanley <stanleyagbanobi@gmail.com>
2024-09-22 20:17:43 +0100

    _document updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,12 @@
 import React from "react";
+
 import { Html, Head, Main, NextScript } from "next/document";
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Include Stripe.js script */}
-        <script src="https://js.stripe.com/v3/"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
